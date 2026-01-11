f8225b53c2280c228490e3f4f309a961a567dcf8
Yunosuke Yamada <yunosuke.yamada@3-shake.com>
2026-01-11 20:53:36 +0900

    create issue

--- README.md
+++ README.md
@@ -9,35 +9,44 @@ uv sync
 Lint
 
 ```
-ruff check --fix
+uv run ruff check --fix
 ```
 
 Format
 
 ```
-ruff format
+uv run ruff format
 ```
 
 Type check
 
 ```
-ty check
+uv run ty check
 ```
 
 Dev server
 
 ```
-fastapi dev main.py
+uv run --env-file .env fastapi dev main.py
 ```
 
 Test
 
 ```
-curl -u test_username:test_password -X POST -H "Content-Type: application/json" -d "{}" http://127.0.0.1:8000
+curl -u test_username:test_password -X POST -H "Content-Type: application/json" --data-binary @payload.json http://127.0.0.1:8000
 ```
 
 Deploy
 
 ```
-gcloud run deploy github-reporter --source . --region asia-northeast1 --allow-unauthenticated
+gcloud run deploy github-reporter --source . --region asia-northeast1 --allow-unauthenticated --env-vars-file=.env
+```
+
+Report error
+
+```
+gcloud beta error-reporting events report --service test-service \
+  --message "java.lang.TestError: msg
+    at com.example.TestClass.test(TestClass.java:51)
+    at com.example.AnotherClass(AnotherClass.java:25)"
 ```
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import os
 import secrets
 from typing import Annotated
 
-from fastapi import Body, Depends, FastAPI, HTTPException, status
+from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from github import Auth, Github
+from pydantic import BaseModel
 
 app = FastAPI()
 security = HTTPBasic()
@@ -18,6 +21,49 @@ def basic_auth(credentials: Annotated[HTTPBasicCredentials, Depends(security)]):
         )
 
 
+class GroupInfo(BaseModel):
+    project_id: str
+    detail_link: str
+
+
+class ExceptionInfo(BaseModel):
+    type: str
+    message: str
+
+
+class EventInfo(BaseModel):
+    log_message: str
+    request_method: str
+    request_url: str
+    referrer: str
+    user_agent: str
+    service: str
+    version: str
+    response_status: str
+
+
+class ErrorReportingWebhook(BaseModel):
+    version: str
+    subject: str
+    group_info: GroupInfo
+    exception_info: ExceptionInfo
+    event_info: EventInfo
+
+
+auth = Auth.Token(os.environ["GITHUB_PAT"])
+
+
 @app.post("/", dependencies=[Depends(basic_auth)])
-def report(payload: dict = Body(...)):
-    print(payload)
+def report(report: ErrorReportingWebhook):
+    with Github(auth=auth) as g:
+        repo = g.get_repo("YunosukeY/error-report-to-github-sample")
+        repo.create_issue(
+            title=report.event_info.log_message,
+            body=f"""エラー
+```
+{report.event_info.log_message}
+```
+
+[ログの詳細を確認]({report.group_info.detail_link})
+""",
+        )
